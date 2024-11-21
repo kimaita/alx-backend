78915ac0c6978158c4b3cfc5e0bb77265da4cf75
kimaita <johnnieqaym@gmail.com>
2024-11-22 00:47:41 +0300

    formatting

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
     """Returns a hello world page"""
-    return render_template('0-index.html')
+    return render_template("0-index.html")
 
-if __name__ == '__main__':  
-   app.run()
+
+if __name__ == "__main__":
+    app.run()
